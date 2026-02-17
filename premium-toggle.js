4b67955831c20fe4a7d2890d7ee3f24234d7dd91
Nitya Prakash Pattanaik <pnityaprakash@gmail.com>
2026-02-17 23:00:18 +0530

    Update QR code URL and menu link in premium toggle

--- premium-toggle.js
+++ premium-toggle.js
@@ -3,7 +3,7 @@
 
 (function () {
   // --- CONFIG ---
-  const qrImgUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=menu.html';
+  const qrImgUrl = 'https://khatti-cafe.vercel.app/menu.html';
   const menuLink = 'menu.html';
 
   // --- BUTTON ---
@@ -189,3 +189,4 @@
     }
   });
 })();
+
